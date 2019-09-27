7de89befd02a39a74bf80e9f6e8c0ffd4ea59afa
Tod Totev <t.totev@corp.edynamix.co.uk>
2019-09-27 15:15:01 +0300

    Add project files.

---
+++ ViceCity/Models/Guns/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViceCity.Models.Guns.Contracts;
+
+namespace ViceCity.Models.Guns
+{
+    class Gun : IGun
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int BulletsPerBarrel => throw new NotImplementedException();
+
+        public int TotalBullets => throw new NotImplementedException();
+
+        public bool CanFire => throw new NotImplementedException();
+
+        public int Fire()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ViceCity/Models/Players/MainPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViceCity.Models.Players
+{
+    public class MainPlayer : Player
+    {
+
+        private const string mainName = "Tommy Vercetti";
+        private const int initialLP = 100;
+
+        public MainPlayer() : base(mainName, initialLP)
+        {
+        }
+    }
+}
