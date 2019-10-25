6701bde0c6d6855d1c1201d1eda7cb309d3efa60
Totyo Totev <totyo666@gmail.com>
2019-10-25 16:31:49 +0300

    ner try

--- ViceCity/Core/Engine.cs
+++ ViceCity/Core/Engine.cs
@@ -1,57 +1,14 @@
-﻿using ViceCity.Core.Contracts;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ViceCity.IO.Contracts;
-using ViceCity.IO;
 
-namespace ViceCity.Core
+namespace ViceCity.Core.Contracts
 {
-    public class Engine : IEngine
+    class Engine : IEngine
     {
-        private IReader reader;
-        private IWriter writer;
-
-
-        public Engine()
-        {
-            this.reader = new Reader();
-            this.writer = new Writer();
-
-        }
         public void Run()
         {
-            while (true)
-            {
-                var input = reader.ReadLine().Split();
-                if (input[0] == "Exit")
-                {
-                    Environment.Exit(0);
-                }
-                try
-                {
-                    if (input[0] == "AddPlayer")
-                    {
-                        
-                    }
-                    else if (input[0] == "AddGun")
-                    {
-
-                    }
-                    else if (input[0] == "AddGunToPlayer")
-                    {
-
-                    }
-                    else if (input[0] == "Fight")
-                    {
-
-                    }            
-                }
-                catch (Exception ex)
-                {
-                    writer.WriteLine(ex.Message);
-                }
-            }
+            throw new NotImplementedException();
         }
     }
 }
